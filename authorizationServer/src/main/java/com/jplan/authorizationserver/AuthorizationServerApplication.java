67db6cb02c0881df622d2ac6496f66fd4eab4750
Jeong yong hoon <jyh.dev@gmail.com>
2021-09-18 20:46:51 +0900

    LOGIN SERVER SETTING

--- authorizationServer/src/main/java/com/jplan/authorizationserver/AuthorizationServerApplication.java
+++ authorizationServer/src/main/java/com/jplan/authorizationserver/AuthorizationServerApplication.java
@@ -2,8 +2,9 @@ package com.jplan.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
