2b706b0f9a7adb69748faedd71bbe721dc293c6f
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-09 22:47:16 +0900

    SMALL UPDATE
    테스트를 위한 업데이트 추후 수정

--- authorizationServer/src/main/java/com/jplan/authorizationserver/dto/LoginInfo.java
+++ authorizationServer/src/main/java/com/jplan/authorizationserver/dto/LoginInfo.java
@@ -1,7 +1,9 @@
 package com.jplan.authorizationserver.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class LoginInfo {
     private String id;
--- authorizationServer/src/main/java/com/jplan/authorizationserver/exceptions/MemberException.java
+++ authorizationServer/src/main/java/com/jplan/authorizationserver/exceptions/MemberException.java
@@ -1,15 +1,29 @@
 package com.jplan.authorizationserver.exceptions;
 
-import lombok.AllArgsConstructor;
-
+//https://luvstudy.tistory.com/50 --> 에러 처리 참고
 /* For testing exception */
-@AllArgsConstructor
 public class MemberException extends RuntimeException {
 
-    private Integer code;
-    private String msg;
+    private Integer errorCode;
+    private String errorMessage;
 
-    public MemberException(Exception memberException) {
+    public MemberException() {
+        super();
+    }
+
+    public MemberException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public MemberException(Integer errorCode) {
+        super("ErrorCode is [" + Integer.toString(errorCode) + "]");
+    }
+
+    public MemberException(Throwable memberException) {
         super(memberException);
+    }
+
+    public MemberException(Integer errorCode, String errorMessage, Throwable memberException) {
+        super("[");
     }
 }
