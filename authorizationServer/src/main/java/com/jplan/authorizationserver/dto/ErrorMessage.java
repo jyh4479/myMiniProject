145358fa501bf250a405eb63444999c752a36139
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-09 22:45:11 +0900

    ADD DTO
    에러 처리를 위한 데이터 객체 생성

---
+++ authorizationServer/src/main/java/com/jplan/authorizationserver/dto/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.jplan.authorizationserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private Integer code;
+    private String message;
+}
