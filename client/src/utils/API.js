d63a3dd317cc0f6d4ceb8d21eaee22502d40226b
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-16 16:29:38 +0900

    UPDATE CLIENT
    정보 확인을 위한 변경

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,11 +4,13 @@ const baseUrl = "http://localhost:8080/";
 const signIn = async (id, password) => {
     const data = {id: id, password: password}
     const res = await axios.post(baseUrl + "jplan/signin", data)
+    console.log(res)
 }
 
 const signUp = async (id, password, name, email) => {
     const data = {id: id, password: password, name: name, email: email}
     const res = await axios.post(baseUrl + "jplan/signup", data)
+    console.log(res)
 }
 
 export default {signIn, signUp}
