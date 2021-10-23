b62abfade1351ba4371ba6c1d408bc8684f4b49c
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-23 22:05:52 +0900

    ADD INTERCEPTOR
    axios에서 지원하는 interceptor를 통해 추후 토큰과 관련된 검증 로직을 추가 예정

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,13 +1,32 @@
 const axios = require('axios')
 const baseUrl = "http://localhost:8080/";
 
+axios.interceptors.request.use((config) => {
+    console.log("req")
+    console.log(config.data)
+    return config
+}, (error) => {
+    console.log("BYE")
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use((response) => {
+    console.log("res")
+    console.log(response.data)
+    return response
+}, (error) => {
+    console.log("BYE")
+    return Promise.reject(error)
+})
+
+
 const signIn = async (id, password) => {
     const data = {id: id, password: password}
-    const res = await axios.post(baseUrl + "jplan/signin", data)
+    const {status} = await axios.post(baseUrl + "jplan/signin", data)
 
     /* 리턴되는 토큰과 로그인 성공 알려주는 로직 추가 */
 
-    return res.status === 200;
+    return status === 200;
 }
 
 const signUp = async (id, password, name, email) => {
