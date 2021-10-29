cf8b5623020b9bb64e30cf3e4fd9a4dc2a325db1
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-29 10:47:59 +0900

    UPDATE ROUTE LOGIC
    Router태그 내부에 조건부 렌더링을 통한 토큰 검증

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
 import {HomePage, LoginPage, SignUpPage} from "./pages";
 
 const App = () => {
-
-    /* 해당 로직 변경 필요 --> 단순 로그인페이지 리턴으로 라우팅이 안먹힘 */
-    if (!window.localStorage.getItem('access-token') && window.location.pathname !== '/login')
-        return <LoginPage/>
-
     return (
         <Router>
+            <Route render={() => (
+                <Redirect to={
+                    !window.localStorage.getItem('access-token') && window.location.pathname !== '/login' ? '/login' : window.location.pathname
+                }/>)}/>
             <Switch>
                 <Route exact path={'/'} component={HomePage}/>
                 <Route path={'/login'} component={LoginPage}/>
