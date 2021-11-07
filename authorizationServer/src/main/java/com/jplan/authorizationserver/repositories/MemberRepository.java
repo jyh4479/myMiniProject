483cce5be22248de5b061a2c15dcdb72cc6ad018
Jeong yong hoon <jyh.dev@gmail.com>
2021-11-07 20:41:09 +0900

    ADD ANNOTATION
    Repository 어노테이션 추가

--- authorizationServer/src/main/java/com/jplan/authorizationserver/repositories/MemberRepository.java
+++ authorizationServer/src/main/java/com/jplan/authorizationserver/repositories/MemberRepository.java
@@ -2,7 +2,8 @@ package com.jplan.authorizationserver.repositories;
 
 import com.jplan.authorizationserver.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 }
--- memberServer/src/main/java/com/jplan/memberserver/repositories/MemberRepository.java
+++ memberServer/src/main/java/com/jplan/memberserver/repositories/MemberRepository.java
@@ -2,7 +2,8 @@ package com.jplan.memberserver.repositories;
 
 import com.jplan.memberserver.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
 }
