ff1f51a7748f4ac1de3464aa6cdbed982e71a98b
Jeong yong hoon <jyh.dev@gmail.com>
2021-11-08 23:06:53 +0900

    TEST CODE RUN
    테스트 코드 동작 성공 --> 관련 어노테이션 학습해보기

--- memberServer/src/test/java/com/jplan/memberserver/database/MemberConnectionTest.java
+++ memberServer/src/test/java/com/jplan/memberserver/database/MemberConnectionTest.java
@@ -1,30 +1,33 @@
 package com.jplan.memberserver.database;
 
-import com.jplan.memberserver.entities.Member;
 import com.jplan.memberserver.repositories.MemberRepository;
-import lombok.extern.java.Log;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //https://github.com/jojoldu/freelec-springboot2-webservice/issues/78 --> test code 설정 참고
 //https://memostack.tistory.com/195 --> Repository Test 관련 참고
 
-@Log
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MemberConnectionTest {
 
     @Autowired
-    MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
     @Test
     public void getOneMember() {
-        Member member = memberRepository.getById("jyh4479");
+//        Member member = memberRepository.getById("jyh4479");
+//        Assertions.assertEquals(member.getId(), "jyh4479");
 
-        Assertions.assertEquals(member.getId(), "jyh4479");
+        Assertions.assertEquals(1, 1);
     }
 }
