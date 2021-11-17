e3edbfead4629b26697f14c2a32d141bc0c003f1
Jeong yong hoon <jyh.dev@gmail.com>
2021-11-17 11:13:01 +0900

    UPDATE TEST CODE
    멤버 서버 Controller Test Code 작성 --> 통신 성공 했고 테스트 관련 어노테이션 + 코드 학습하기

--- memberServer/src/test/java/com/jplan/memberserver/database/MemberControllerTest.java
+++ memberServer/src/test/java/com/jplan/memberserver/database/MemberControllerTest.java
@@ -1,23 +1,28 @@
 package com.jplan.memberserver.database;
 
 import com.jplan.memberserver.controllers.MemberController;
+import com.jplan.memberserver.services.MemberService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
 public class MemberControllerTest {
 
     @Autowired
-    private MemberController memberController;
+    MockMvc mvc;
+
+    @MockBean
+    MemberService memberService;
 
     @Test
-    public void MemberControllerTest() throws Exception{
-
+    public void MemberControllerResponseTest() throws Exception {
+        mvc.perform(get("/jplan/memberservice/member").param("id", "jyh4479"))
+                .andExpect(status().isOk());
     }
 }
