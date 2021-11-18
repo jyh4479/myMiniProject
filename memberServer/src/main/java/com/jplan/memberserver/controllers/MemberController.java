556a0884ed7ada6bb1bdd6c8a2064df78b39b046
Jeong yong hoon <jyh.dev@gmail.com>
2021-11-18 23:37:59 +0900

    UPDATE CONTROLLER

--- memberServer/src/main/java/com/jplan/memberserver/controllers/MemberController.java
+++ memberServer/src/main/java/com/jplan/memberserver/controllers/MemberController.java
@@ -1,9 +1,9 @@
 package com.jplan.memberserver.controllers;
 
+import com.jplan.memberserver.entities.Member;
 import com.jplan.memberserver.services.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +18,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member")
-    public ResponseEntity<?> getMemberData(@RequestParam("id") String id) {
+    public Member getMemberData(@RequestParam("id") String id) {
         log.info("run getmemberData");
-        System.out.println(memberService.getMemberData(id));
-        return null;
+        Member member = memberService.getMemberData(id);
+        System.out.println(member);
+        return member;
     }
 }
