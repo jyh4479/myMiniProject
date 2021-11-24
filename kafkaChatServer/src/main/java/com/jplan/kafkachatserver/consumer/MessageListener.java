386b2d47063e76f95b7f3a2578c20c82e06c8556
Jeong yong hoon <jyh.dev@gmail.com>
2021-11-24 22:13:48 +0900

    Update kafka server
    카프카 예제를 참고한 카피 프로젝트 + 이해하고 커스텀해보기

---
+++ kafkaChatServer/src/main/java/com/jplan/kafkachatserver/consumer/MessageListener.java
@@ -0,0 +1,26 @@
+package com.jplan.kafkachatserver.consumer;
+
+import com.jplan.kafkachatserver.constants.KafkaConstants;
+import com.jplan.kafkachatserver.entities.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MessageListener {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        log.info("sending via kafka listener..");
+        template.convertAndSend("/topic/group", message);
+    }
+}
